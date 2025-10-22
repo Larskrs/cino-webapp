49038388dfaaaacb44de3e162a660958842c4ec2
Larskrs <lkssyv@gmail.com>
2025-10-22 14:01:29 +0200

    change image component

--- src/app/(dashboard)/board/[id]/_cards/image/View.tsx
+++ src/app/(dashboard)/board/[id]/_cards/image/View.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Image from "next/image";
+
 export default function TextCardView({ card }: { card: any }) {
   return (
     <div className="text-sm pointer-events-none text-neutral-600 min-w-60 w-full whitespace-pre-wrap break-words select-none">
-      <img src={card.content} alt="Click to select" className="rounded-0 w-full h-full object-cover" />
+      <Image width={400} height={400} src={card.content} alt="Click to select" className="rounded-0 w-full h-full object-cover" />
     </div>
   );
 }
